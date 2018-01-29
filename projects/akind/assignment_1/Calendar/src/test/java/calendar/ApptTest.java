9bc1172c8f62c1b433e45b98261c71e9e372360a
akinde <akind@oregonstate.edu>
2018-01-28 21:31:00 -0800

    Added JUnit tests for assignment 2

--- projects/akind/assignment_1/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/akind/assignment_1/Calendar/src/test/java/calendar/ApptTest.java
@@ -1,21 +1,19 @@
 package calendar;
-/**
- *  This class provides a basic set of test cases for the
- *  Appt class.
- */
-import org.junit.Test;
 
 import static org.junit.Assert.*;
-public class ApptTest {
-    /**
-     * Test that the gets methods work as expected.
-     */
-	 @Test
-	  public void test01()  throws Throwable  {
-	 }
 
-	 @Test
-	  public void test02()  throws Throwable  {
-		 
-	 }	
+import org.junit.Test;
+
+public class ApptTest {
+
+	@Test
+	public void testAppt() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		assertEquals(A.getStartHour(), 15);
+		assertEquals(A.getStartMinute(), 5);
+		assertEquals(A.getStartMonth(), 5);
+		assertEquals(A.getStartDay(), 5);
+		assertEquals(A.getStartYear(), 2017);
+	}
+
 }
