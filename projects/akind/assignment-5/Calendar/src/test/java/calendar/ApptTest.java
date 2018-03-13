7a84d830021b3061ae235c4b2b933caa69348e37
akinde <akind@oregonstate.edu>
2018-03-12 22:24:47 -0700

    Adding all the files

---
+++ projects/akind/assignment-5/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,163 @@
+package calendar;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class ApptTest {
+
+	@Test
+	public void testGetters() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		assertEquals(A.getStartHour(), 15);
+		assertEquals(A.getStartMinute(), 5);
+		assertEquals(A.getStartMonth(), 5);
+		assertEquals(A.getStartDay(), 5);
+		assertEquals(A.getStartYear(), 2017);
+		assertEquals(A.getTitle(), "Test");
+		assertEquals(A.getDescription(), "Default");
+		assertEquals(A.getRecurNumber(), 0);
+		assertEquals(A.getRecurBy(), 2);
+		assertFalse(A.isRecurring());
+		assertEquals(A.getRecurIncrement(), 0);
+	}
+	@Test
+	public void testSetters() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		A.setStartHour(16);
+		A.setStartMinute(6);
+		A.setStartMonth(6);
+		A.setStartDay(6);
+		A.setStartYear(2018);
+		A.setTitle("SetterTest");
+		A.setDescription("Default2");
+		assertEquals(A.getStartHour(), 16);
+		assertEquals(A.getStartMinute(), 6);
+		assertEquals(A.getStartMonth(), 6);
+		assertEquals(A.getStartDay(), 6);
+		assertEquals(A.getStartYear(), 2018);
+		assertEquals(A.getTitle(), "SetterTest");
+		assertEquals(A.getDescription(), "Default2");
+		A.setTitle(null);
+		assertEquals(A.getTitle(), "");
+		A.setDescription(null);
+		assertEquals(A.getDescription(), "");
+	}
+	
+
+	@Test
+	public void testHour() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		A.setStartHour(13);
+		assertTrue(A.getValid());
+		A.setStartHour(25);
+		assertFalse(A.getValid());
+	}
+	
+	@Test
+	public void testMinute() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		A.setStartMinute(-1);
+		assertFalse(A.getValid());
+		A.setStartMinute(61);
+		assertFalse(A.getValid());
+	}
+	
+	@Test
+	public void TestMonth() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		A.setStartDay(0);
+		assertFalse(A.getValid());
+		A.setStartDay(33);
+		assertFalse(A.getValid());
+	}
+	
+	@Test
+	public void TestYear() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		assertTrue(A.getValid());
+	}
+	@Test
+	public void testRecurrance() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		int[] recurringDays = new int[0];
+		assertEquals(A.getRecurNumber(), 0);
+		assertEquals(A.getRecurBy(), 2);
+		assertFalse(A.isRecurring());
+		assertEquals(A.getRecurIncrement(), 0);
+		assertArrayEquals(A.getRecurDays(), recurringDays);
+		recurringDays = null;
+        A.setRecurrence(recurringDays, 0, 0, 2);
+        recurringDays = new int[0];
+		assertArrayEquals(A.getRecurDays(), recurringDays); 
+		assertTrue(A.isRecurring());
+	}
+	@Test
+	public void testToString() {
+		Appt A = new Appt(15, 5, 5, 5, 2017, "Test", "Default");
+		A.setStartHour(13);
+		assertNotNull(A.toString());
+		A.setStartHour(-1);
+		assertNull(A.toString());
+	}
+	
+	@Test
+	public void testIsValid() {
+	Appt A = new Appt(12, 5, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(14, 5, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 5, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(20, 5, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(23, 5, 5, 5, 2018, "Test", "Test");
+	//assertTrue(A.getValid());
+	A = new Appt(25, 5, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(27, 5, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	
+	A = new Appt(16, -2, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, -1, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, 1, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 25, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 35, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 45, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 59, 5, 5, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 60, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, 75, 5, 5, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	
+	A = new Appt(16, 25, -10, 1, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, 25, -1, 1, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, 25, 1, 1, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 25, 10, 1, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 25, 20, 1, 2018, "Test", "Test");
+	assertTrue(A.getValid());
+	A = new Appt(16, 25, 32, 1, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	A = new Appt(16, 25, 60, 2, 2018, "Test", "Test");
+	assertFalse(A.getValid());
+	
+	}
+	
+}
+
+
+
+
+
---
+++ projects/akind/assignment-5/Calendar/src/test/java/calendar/CalendarUtilTest.java
@@ -0,0 +1,26 @@
+package calendar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalendarUtilTest {
+
+	@Test
+	public void testIsLeapYear() {
+		assertTrue(CalendarUtil.IsLeapYear(29200));
+		assertFalse(CalendarUtil.IsLeapYear(73));
+		assertFalse(CalendarUtil.IsLeapYear(146));
+		assertTrue(CalendarUtil.IsLeapYear(4));
+		assertFalse(CalendarUtil.IsLeapYear(5));
+	}
+	
+	@Test
+	public void testNumDaysInMonth() {
+		assertEquals(29, CalendarUtil.NumDaysInMonth(4, 1));
+		//assertEquals(28, CalendarUtil.NumDaysInMonth(5, 1));
+		assertEquals(31, CalendarUtil.NumDaysInMonth(5, 2));
+		assertEquals(32, CalendarUtil.NumDaysInMonth(4, 2));
+	}
+
+}
